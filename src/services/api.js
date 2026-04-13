67884dc1f9f252ebe86f833f9b50e8b9c8a72bd0
PavaniMutyala <pavanimutyala44@gmail.com>
2026-04-13 22:13:48 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://aidmap-project.onrender.com/api';
 
 export const fetchVolunteers = async () => {
   const res = await fetch(`${API_BASE_URL}/volunteers`);
